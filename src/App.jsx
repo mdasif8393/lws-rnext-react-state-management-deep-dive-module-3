52a268354aef8dd5d21862aaf3a40a80d3ec7fee
mdasif8393 <md.asif.8393@gmail.com>
2025-03-16 11:18:35 +0600

    3.11 Resetting state at the same position - Changing Default behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import Accordion from "./components/Accordion";
-import Travel from "./components/Travel";
-import TravelPlan from "./components/TravelPlan";
+import "./style.css";
+import { useState } from "react";
 
-export default function App() {
+export default function Scoreboard() {
+  const [isPlayerA, setIsPlayerA] = useState(true);
   return (
     <div>
-      <Accordion />
+      {isPlayerA ? (
+        <Counter person="Taylor" key="Taylor" />
+      ) : (
+        <Counter person="Sarah" key="Sarah" />
+      )}
+      <button
+        onClick={() => {
+          setIsPlayerA(!isPlayerA);
+        }}
+      >
+        Next player!
+      </button>
+    </div>
+  );
+}
+
+function Counter({ person }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = "counter";
+  if (hover) {
+    className += " hover";
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>
+        {person}'s score: {score}
+      </h1>
+      <button onClick={() => setScore(score + 1)}>Add one</button>
     </div>
   );
 }
